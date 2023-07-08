5f6413d1ef032ab4d7ca91955838f7278ea1af37
Python-Turtle <133562016+PythonTurtle02@users.noreply.github.com>
2023-07-08 15:12:42 +0530

    Add files via upload

---
+++ space_handle.py
@@ -0,0 +1,19 @@
+name1 = "dhayajune"
+name2 = "uzumaki naruto"
+name3 = "obito"
+name4 = "gaara"
+name5 = "uchiha maadara"
+name6 = "itachi"
+name7 = "shikamaru"
+name8 = "konan"
+name9 = "jiraiya"
+
+print(f'{name1:18} : 09')
+print(f'{name2:18} : 04')
+print(f'{name3:18} : 05')
+print(f'{name4:18} : 06')
+print(f'{name5:18} : 03')
+print(f'{name6:18} : 01')
+print(f'{name7:18} : 08')
+print(f'{name8:18} : 07')
+print(f'{name9:18} : 02')
